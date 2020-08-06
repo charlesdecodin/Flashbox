b9e0b67432bf752e4e05a4d21f88140d1d383637
charles decodin <charles.decodin@outlook.fr>
2020-08-06 17:08:38 +0200

    create component reusable

--- site/src/components/InputList.jsx
+++ site/src/components/InputList.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import '../style/inputList.css'
+import Context from '../context/context'
 
-export default function InputList() {
+export default function InputList({state, setState, path, value}) {
+    const {server} = useContext(Context)
+    
+    const token = localStorage.getItem('token') || sessionStorage.getItem('token')
+
+   useEffect(() => {
+    const getCategories = async () =>{
+        const config = {
+            headers : { 
+                'x-access-token': token
+            },
+        }
+        const promise = await fetch(`${server}/${path}`, config)
+        const content = await promise.json()
+        console.log(content);
+        setState(content)
+    }
+    getCategories()
+   }, [])
+   console.log(state);
+   
     return (
         <select className="" name="" id="">
-            <option value="">Catégorie</option>
-            <option value="">Anglais</option>
-            <option value="">Javascript</option>
+            {state.map((item, index)=>{
+                return(
+                     <option key={index}>{item.noun}</option>
+                )
+            })}
         </select>
     )
 }
