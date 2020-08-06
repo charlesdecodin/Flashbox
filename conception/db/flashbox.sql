1acf976e1bc96b948652d041d6421191879de32a
charles decodin <charles.decodin@outlook.fr>
2020-08-06 11:22:55 +0200

    create table account_category, category, squad and card

--- conception/db/flashbox.sql
+++ conception/db/flashbox.sql
@@ -3,8 +3,45 @@ CREATE EXTENSION IF NOT EXISTS "uuid_ossp";
 CREATE TABLE account
 (
     account_id UUID PRIMARY KEY, 
-    first_name varchar(30),
-    last_name varchar(30),
-    email varchar(100) UNIQUE NOT NULL,
-    password varchar(64)
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(64)
+);
+
+CREATE TABLE category
+(
+    category_id UUID PRIMARY KEY,
+    category_name VARCHAR(30),
+    primary_color INTEGER,
+    secondary_color INTEGER
+);
+
+CREATE TABLE account_categorie
+(
+    account_category_id UUID PRIMARY KEY,
+    account_id UUID,
+    category_id UUID,
+    FOREIGN KEY (account_id) REFERENCES account (account_id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE
+);
+
+CREATE TABLE squad
+(
+    squad_id UUID PRIMARY KEY,
+    squad_name VARCHAR(30),
+    primary_color INTEGER,
+    secondary_color INTEGER,
+    FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE
+);
+
+CREATE TABLE card
+(
+    card_id UUID PRIMARY KEY,
+    recto VARCHAR(100),
+    verso VARCHAR(100),
+    evaluation BIT,
+    card_rank INTEGER,
+    last_update TIME,
+    FOREIGN KEY (squad_id) REFERENCES squad (squad_id) ON DELETE CASCADE
 );
